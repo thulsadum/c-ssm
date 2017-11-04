a13cd6f9934a81586ba62644772bcc4493b39371
Sebastian <sc@inferum.net>
2017-11-04 22:56:41 +0100

    Postponed segment concatenation: might be irrelevant

--- src/segment.h
+++ src/segment.h
@@ -50,35 +50,24 @@ int         cpy_segment ( Segment_t * dst, const Segment_t * src );
 #define _del_segment(PPSEG) del_segment( (Segment_t**) PPSEG )
 #define _cpy_segment(PDST, PSRC, S) \
      cpy_segment( ((Segment_t*) PDST), ((Segment_t*) PSRC), S ) 
-Segment_t * segment_concat ( Segment_t * pFst, Segment_t * pSnd ); 
-#define _segment_concat(T, PSEG1, PSEG2) \
-     ((T) segment_concat( ((Segment_t*) PSEG1), ((Segment_t*) PSEG2) ))
 
 
 Segment_Text_t * new_segment_text ( size_t i );
 void del_segment_text ( Segment_Text_t ** ppSeg );
 int cpy_segment_text ( Segment_Text_t * dst, const Segment_Text_t * src );
 int segment_add_text ( Segment_Text_t **ppSeg, const char * text );
-#define segment_text_concat(SEG1, SEG2) \
-     _segment_concat(Segment_Text_t*, SEG1, SEG2)
 
 
 Segment_Data_t * new_segment_data ( size_t i );
 #define del_segment_data(PPSEG) _del_segment(PPSEG) 
 int     cpy_segment_data(Segment_Data_t *dst, Segment_Data_t *src);
 int segment_add_data ( Segment_Data_t **ppSeg, int64_t data);
-#define segment_data_concat(SEG1, SEG2) \
-     _segment_concat(Segment_Data_t*, SEG1, SEG2)
 
 
 Segment_Code_t * new_segment_code ( size_t i );
 #define del_segment_code(PPSEG) _del_segment(PPSEG) 
 int     cpy_segment_code(Segment_Code_t *dst, Segment_Code_t *src);
 int segment_add_code ( Segment_Code_t **ppSeg, Semantic_t code);
-#define segment_code_concat(SEG1, SEG2) \
-     _segment_concat(Segment_Code_t*, SEG1, SEG2)
 
-
-/* TODO */
 
 #endif
